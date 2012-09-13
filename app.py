8869cf7935586bd325ca10eac4bda8f4e9fc0e6a
John Schimmel <john.schimmel@gmail.com>
2012-09-13 16:00:24 -0400

    adding /page2 and debug to our flask app

--- app.py
+++ app.py
@@ -1,9 +1,21 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask # Retrieve Flask, our framework
+app = Flask(__name__)   # create our flask app
 
+# this is our main page
 @app.route("/")
-def hello():
-    return "Hello World!"
+def index():
+    return """Hello World!
+    <br/><br/>
+    <a href='/page2'>Visit page #2</a>"""
 
+
+# this is the 2nd route - can be access with /page2
+@app.route("/page2")
+def page2():
+	return "<h2>Welcome to page 2</h2><p>This is just amazing!</p>"
+
+
+# start the webserver
 if __name__ == "__main__":
-    app.run()
+	app.debug = True
+	app.run()
